bc30f903fe83f6caf2b4742a7bb69113cd4a7d82
Hao-Bo Guo <guohaobo@gmail.com>
2018-11-10 18:12:29 -0500

    Pathways

---
+++ Data/pathway.R
@@ -0,0 +1,20 @@
+####
+#library('data.table')
+rm(list=ls())
+human.pathway <- read.csv("human.pathways.table.txt", sep="\t", header=T, stringsAsFactors = F)
+pathway <- human.pathway$pathway
+resource <- human.pathway$source
+gene <- human.pathway$gene
+
+pair <- c("Source", "Pathway", "Gene")
+
+for (i in 1:length(pathway)) {
+  path <- as.character(pathway[i])
+  as.character(reso <- resource[i])
+  gene.expand <- as.character(unlist(strsplit(as.character(gene[i]), ",")))
+     for (j in 1:length(gene.expand)) {
+        pair <- rbind(pair, c(reso, path, gene.expand[j]))
+  }
+}
+
+write.table(pair, file="human.pathway.csv", sep=",", row.names = F, col.names=F, quote=F)
